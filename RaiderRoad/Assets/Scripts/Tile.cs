ca51e03de104e86e95fd60b8f307123268b344fc
Niralius <michaelheffnercruz@gmail.com>
2018-10-19 11:11:40 -0400

    Prefab work, and started item dropping

---
+++ RaiderRoad/Assets/Scripts/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public GameObject wall;
+    public float height = 1f;
+    public bool isHorizontal;
+    public float health;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown(){
+        // this object was clicked - do something
+        if(this.isHorizontal)
+        {
+            Instantiate(wall, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(wall, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 90, 0)));
+        }
+    }
+}
