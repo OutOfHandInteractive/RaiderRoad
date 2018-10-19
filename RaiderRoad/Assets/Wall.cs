74b9b2bf7d9cbd70fa199301c4c8cf72a153fef2
Niralius <michaelheffnercruz@gmail.com>
2018-10-19 11:57:49 -0400

    item dropping

--- RaiderRoad/Assets/Wall.cs
+++ RaiderRoad/Assets/Wall.cs
@@ -17,6 +17,15 @@ public class Wall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(hits <= 0 || health <= 0)
+        {
+            spawn();
+        }
 	}
+
+    void spawn()
+    {
+        Instantiate(drop, new Vector3(transform.position.x, transform.position.y - .5f, transform.position.z), Quaternion.identity);
+        Destroy(this.gameObject);
+    }
 }
