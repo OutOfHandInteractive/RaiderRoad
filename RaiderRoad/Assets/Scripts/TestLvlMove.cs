2bca7c52a7ba735625f6c7896bf35f60e247ddfb
Fatality233 <39014054+Fatality233@users.noreply.github.com>
2018-10-21 00:04:04 -0400

    lvl scenes added

---
+++ RaiderRoad/Assets/Scripts/TestLvlMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLvlMove : MonoBehaviour {
+
+	public GameObject road;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * 40 * Time.deltaTime);
+	}
+}
