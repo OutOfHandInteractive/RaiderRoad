738567edb9a32eac0cf5101345d7a27c7da4541f
Jeffrey Mostyn <jeffmostyn2@gmail.com>
2018-11-03 21:40:25 -0400

    phuck physics

--- RaiderRoad/Assets/Scripts/Vehicle Weapons/cannonball.cs
+++ RaiderRoad/Assets/Scripts/Vehicle Weapons/cannonball.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cannonball : MonoBehaviour {
-
+	
 	public float damage;
 	public float travelTime;
 
@@ -11,12 +11,22 @@ public class cannonball : MonoBehaviour {
 	private Vector3 source;
 	private Vector3 dir;
 	private Vector3 horizVelocity;
+	private float vertSpeed;
 	private float timeRemaining;
-	
+
+	// Projectile motion
+	private const float ANGLE = 45;
+	private const float G = -9.81f;
+	private float timeElapsed;
+	private float oldY;
+
 	// Update is called once per frame
 	void Update () {
 		transform.position += horizVelocity * Time.deltaTime;
 		timeRemaining -= Time.deltaTime;
+		timeElapsed = travelTime - timeRemaining;
+
+		//transform.position += new Vector3(0f, deltaY(), 0f);
 
 		if(timeRemaining <= 0) {
 			Destroy(gameObject);
@@ -28,7 +38,14 @@ public class cannonball : MonoBehaviour {
 		source = _source;
 		dir = target - source;
 		horizVelocity = dir / travelTime;
+		vertSpeed = horizVelocity.magnitude * Mathf.Tan(ANGLE);
 
 		timeRemaining = travelTime;
+	}
+
+	public float deltaY() {
+		Debug.Log(vertSpeed);
+
+		return (float)(source.y + (vertSpeed * timeElapsed) + (0.5 * G * Mathf.Pow(timeElapsed, 2))) - transform.position.y;
 	}
 }
