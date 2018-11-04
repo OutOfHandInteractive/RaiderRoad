22cd97e74e44ff100bd13718891f1b039b6573e5
William Brown <wbrown026@gmail.com>
2018-11-04 05:22:39 -0500

    Added Flame Thrower
    Works much like the cannon, but has an overheating mechanic and a limited range of field.

---
+++ RaiderRoad/Assets/flame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flame : MonoBehaviour {
+
+    public float speed = 50f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right, speed * Time.deltaTime);
+    }
+
+}
