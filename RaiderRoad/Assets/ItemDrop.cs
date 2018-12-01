e136834d6a2067de9026bec3f682b61b4326bdde
ep429 <pambukue3@gmail.com>
2018-12-01 03:07:30 -0500

    Weapons work on EnemyAI scene, parenting drops to vehicles
    Still need to get shooting working for enemyAI scene, rewired thing most
    likely

--- RaiderRoad/Assets/ItemDrop.cs
+++ RaiderRoad/Assets/ItemDrop.cs
@@ -5,4 +5,27 @@ using UnityEngine;
 public class ItemDrop : MonoBehaviour {
     //contains a reference to the item it will bestow the player
     public GameObject item;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "floor")
+        {
+            transform.parent = other.transform;
+            transform.localPosition = new Vector3(0, 0, 0);
+        }
+        if(other.gameObject.tag == "Destructable")
+        {
+            transform.parent = other.transform.root;
+            transform.localPosition = new Vector3(0, .5f, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "floor" || other.gameObject.tag == "Destructable")
+        {
+            transform.parent = null;
+        }
+    }
 }
