c68b2cdb463841f4426bc67673a88691b3f4bc99
William Brown <wbrown026@gmail.com>
2018-12-01 22:18:23 -0500

    Obstacle Warning Arrows follow rv camera properly while driving

---
+++ RaiderRoad/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    private GameObject rv;
+    private float rvZStart;
+    private float warningZStart;
+
+	void Awake() {
+        warningZStart = transform.position.z;
+	}
+	
+	void Update () {
+        float newZ = warningZStart + (rv.transform.position.z - rvZStart);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
+	}
+
+    public void SetValues(float value, GameObject other)
+    {
+        rvZStart = value;
+        rv = other;
+    }
+}
--- RaiderRoad/Assets/Scripts/SpawnRoad.cs
+++ RaiderRoad/Assets/Scripts/SpawnRoad.cs
@@ -10,23 +10,27 @@ public class SpawnRoad : MonoBehaviour {
     public float speed;
     public float FirstSpawnOffset = 10f;
     public GameObject warningSprite;
+    public GameObject rv;
 
     //--------------------
     // Private Variables
     //--------------------
     private int next;
     private List<GameObject> warnings = new List<GameObject>();
+    private float rvZStart;
+    private BoxCollider col;
 
     // Use this for initialization
     void Start () {
+        rvZStart = rv.transform.position.z;
+        col = gameObject.GetComponent<BoxCollider>();
         Vector3 offset = new Vector3(transform.position.x, transform.position.y, transform.position.z + FirstSpawnOffset);
         GameObject road = Instantiate(roadSegments[0], offset, roadSegments[0].transform.rotation);
         road.GetComponent<MoveRoad>().SetSpeed(speed);
         next = 1;
     }
-	
-	// Update is called once per frame
-	public void Spawn() {
+
+    public void Spawn() {
         if (next >= roadSegments.Count) {
             GameObject road = Instantiate(roadSegments[0], transform.position, roadSegments[0].transform.rotation);
             road.GetComponent<MoveRoad>().SetSpeed(speed);
@@ -44,6 +48,7 @@ public class SpawnRoad : MonoBehaviour {
         if (other.gameObject.tag.Equals("Obstacle"))
         {
             warnings.Add(Instantiate(warningSprite, new Vector3(other.transform.position.x, warningSprite.transform.position.y, warningSprite.transform.position.z), Quaternion.identity));
+            warnings[warnings.Count - 1].GetComponent<FollowCamera>().SetValues(rvZStart, rv);
         }
     }
 
