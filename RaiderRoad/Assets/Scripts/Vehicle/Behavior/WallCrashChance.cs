67edb14d5598206f93c59062252fe0e7b5aa66fc
ep429 <pambukue3@gmail.com>
2018-12-01 19:23:47 -0500

    Road spawn speed decreased, wall crash chance

---
+++ RaiderRoad/Assets/Scripts/Vehicle/Behavior/WallCrashChance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WallCrashChance : MonoBehaviour {
+
+    private int hitChance;
+    private NavMeshObstacle obstacle;
+	// Use this for initialization
+	void Start () {
+        hitChance = Random.Range(0, 100);
+        obstacle = GetComponent<NavMeshObstacle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(hitChance < 80)
+        {
+            obstacle.enabled = true;
+        }
+        else
+        {
+            obstacle.enabled = false;
+        }
+	}
+}
