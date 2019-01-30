1fb27d6088f5ea6d92211b78b4c533d1dcac798c
Jeffrey Mostyn <jeffmostyn2@gmail.com>
2019-01-29 23:48:26 -0500

    armor should impact part health now

--- RaiderRoad/Assets/Scripts/Vehicle/Parts/Attachment.cs
+++ RaiderRoad/Assets/Scripts/Vehicle/Parts/Attachment.cs
@@ -3,11 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Attachment : DestructiblePart {
-
-	public float ramDamageStacks;
-	public int armorStacks;
-	public int speedStacks;
-	public float threatModifier;
-
 	protected abstract override float GetMaxHealth();
 }
--- RaiderRoad/Assets/Scripts/Vehicle/Parts/Cab.cs
+++ RaiderRoad/Assets/Scripts/Vehicle/Parts/Cab.cs
@@ -9,10 +9,5 @@ public abstract class Cab : DestructiblePart {
 	// references
 	public GameObject cargoNode, front_attachmentNode;
 
-	// gameplay values
-	public int armorStacks;
-	public int speedStacks;
-	public float threatModifier;
-
 	protected abstract override float GetMaxHealth();
 }
