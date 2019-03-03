e5a5c62332dad0a9017d69d32316b1e5140c7b57
ep429 <pambukue3@gmail.com>
2019-03-02 22:12:36 -0500

    Revert "better driving, again, can now 'grind' enemies on walls"
    This reverts commit 6272406af8f2560f013b096108786d6e00e9403f.

---
+++ RaiderRoad/Assets/Scripts/Occupied.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to hold an occupied bool
+/// </summary>
+public class Occupied : MonoBehaviour
+{
+    public bool isOccupied;
+}
