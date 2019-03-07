98f8f69fea3018119c271e60a90866c75aeca8c3
Tobias Highfill <colossuskid@gmail.com>
2019-03-07 17:39:59 -0500

    Slight tweaks

--- RaiderRoad/Assets/Scripts/Vehicle/Parts/Payload.cs
+++ RaiderRoad/Assets/Scripts/Vehicle/Parts/Payload.cs
@@ -17,19 +17,27 @@ public abstract class Payload : MonoBehaviour {
         List<GameObject> payloadInstance = new List<GameObject>();
         for (int i = 0; i < GetSize(); i++)
         {
+            Vector3 localPos = Vector3.zero;
             if (payloadCode[i] == payloadTypes.enemy)
             {
-                payloadInstance.Add(Instantiate(SelectEnemies().gameObject, nodes[i].transform));
+                payloadInstance.Add(Instantiate(SelectEnemies().gameObject, nodes[i].transform.position, Quaternion.identity, nodes[i].transform));
+                // HACK HACK HACK!!
+                //localPos = new Vector3(0, -0.5f, 0.5f);
             }
             else if (payloadCode[i] == payloadTypes.weapon)
             {
                 payloadInstance.Add(Instantiate(SelectInteractable().gameObject, nodes[i].transform));
+                // HACK HACK HACK!!
+                if(payloadInstance[i].GetComponentInChildren<cannon>() != null)
+                {
+                    localPos = new Vector3(0, 0.414f, 0);
+                }
             }
             else
             {
                 throw new System.Exception("Unknown payload code: " + payloadCode[i]);
             }
-            payloadInstance[i].transform.localPosition = Vector3.zero;
+            payloadInstance[i].transform.localPosition = localPos;
             //payloadInstance[i].transform.position += payloadOffset(i);
         }
         return payloadInstance;
