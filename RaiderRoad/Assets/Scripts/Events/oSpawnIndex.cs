ce0b9389e01ee154a4c6cc9a0402f46307fbca9b
Tom <tcd53@drexel.edu>
2019-04-16 00:53:21 -0400

    framework for limiting
    idk why obstacles don't keep spawning but I'll get to the bottom of this

---
+++ RaiderRoad/Assets/Scripts/Events/oSpawnIndex.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oSpawnIndex : MonoBehaviour
+{
+    public int index;
+    public GameObject manager;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "RV")
+        {
+            Debug.Log("collision detected");
+            manager.GetComponent<obstacleSpawner>().rvIndex(index);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- RaiderRoad/Assets/Scripts/Events/obstacleSpawner.cs
+++ RaiderRoad/Assets/Scripts/Events/obstacleSpawner.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class obstacleSpawner : MonoBehaviour
 {
 
-    public float startDelay = 30f;
-    public float obstDelay = 23f;
+    public float startDelay;//set to 30f in editor
+    public float obstDelay;//set to 23f in editor
     [SerializeField]
-    private GameObject obstacle;
+    private GameObject smallObstacle;
     public GameObject oSpawnsParent;
     [SerializeField]
     private List<Transform> ospawnPoints;
+    private int startPos = 2;   //initial "lane" the RV starts in
+    [SerializeField]
+    private int rvPos = 0;
+    private int upperBound;
+    private int lowerBound;
 
     // Start is called before the first frame update
     void Start()
     {
+        rvPos = startPos;
         foreach (Transform child in oSpawnsParent.transform)      //get obstacle spawn points
         {
             //Debug.Log(child);
@@ -37,15 +43,45 @@ public class obstacleSpawner : MonoBehaviour
         yield return new WaitForSeconds(obstDelay);    
     }
 
+    public void rvIndex(int curIndex)
+    {
+        rvPos = curIndex;
+    }
+
     public void oSpawn()
     {
-        int i = Random.Range(0, ospawnPoints.Count);
+        //Debug.Log("oSpawn called");
+        //GameObject obstacle = new GameObject();
+        //int randNum = UnityEngine.Random.Range(0,1);
+        //if(randNum == 0){   //small obstacle
+            //obstacle = smallObstacle;
+        //}
+        //else{
+            //obstacle = largeObstacle;
+        //}
+        
+        if(rvPos == 0){
+            //i = Random.Range(rvPos,rvPos + 1);
+            upperBound = rvPos + 1;
+            lowerBound = rvPos;
+        }else if(rvPos == 4){
+            //i = Random.Range(rvPos - 1, rvPos);
+            upperBound = rvPos;
+            lowerBound = rvPos - 1;
+        }else{
+            //i = Random.Range(rvPos - 1,rvPos + 1); // ospawnPoints.Count);        
+            upperBound = rvPos + 1;
+            lowerBound = rvPos - 1;
+        }
+        int i = Random.Range(lowerBound, upperBound);
+        //Debug.Log(i);
         Vector3 spawnPoint = ospawnPoints[i].transform.position;
-        if (obstacle != null)
+        if (smallObstacle != null)
         {
-            GameObject newObstacle = Instantiate(obstacle, spawnPoint, Quaternion.identity);    /////need obstacle prefab
+            GameObject newObstacle = Instantiate(smallObstacle, spawnPoint, Quaternion.identity);    /////need obstacle prefab
+            newObstacle.transform.Rotate(0f, -90f, 0f);    //kinda a bullshit fix for now - i'll explain and fix better at testing
             newObstacle.GetComponentInChildren<eventObject>().setCluster(this.gameObject);
-            newObstacle.transform.Rotate(0f, 90f, 0f);    //kinda a bullshit fix for now - i'll explain and fix better at testing
+            
         }
         else
         {
