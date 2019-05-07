7348a980f366471d68311130847db65940beb2b3
MarkH-PC\MarkH <mdh326@drexel.edu>
2019-05-06 22:19:25 -0400

    It works!

---
+++ RaiderRoad/Assets/IKTestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKTestScript : MonoBehaviour
+{
+    Animator myAnim;
+
+    public float leftHandIKPosWeight = 0;
+    public float leftHandIKRotWeight = 0;
+    public Transform leftHandIKObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        myAnim.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftHandIKPosWeight);
+        myAnim.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftHandIKRotWeight);
+        myAnim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandIKObj.position);
+        myAnim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandIKObj.rotation);
+    }
+}
