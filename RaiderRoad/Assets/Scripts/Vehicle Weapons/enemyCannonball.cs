df3dd49cf04d14f24400a4349c74065e3125bca5
Michael Heffner <michaelheffnercruz@gmail.com>
2019-05-10 11:20:20 -0400

    removed collission on flamethrower and cannons destroy walls

--- RaiderRoad/Assets/Scripts/Vehicle Weapons/enemyCannonball.cs
+++ RaiderRoad/Assets/Scripts/Vehicle Weapons/enemyCannonball.cs
@@ -54,7 +54,7 @@ public class enemyCannonball : AbstractCannonball
     }
 
     void OnCollisionEnter(Collision collision)
-    { 
+    {
         //if(Util.isPlayer(collision.gameObject) || collision.gameObject.tag == "wall" || collision.gameObject.tag == "floor" || Util.IsRV(collision.gameObject) || Util.isEnemy(collision.gameObject))
         if (canCollide)
         {
@@ -62,6 +62,7 @@ public class enemyCannonball : AbstractCannonball
             Collider[] splashTargets = Physics.OverlapSphere(transform.position, splashRadius);
             foreach (Collider target in splashTargets)
             {
+
                 int layerMask = 1 << 10; // Ignore Layer NavMesh
                 layerMask = ~layerMask;
 
@@ -69,6 +70,13 @@ public class enemyCannonball : AbstractCannonball
                 Vector3 dir = (target.transform.position - transform.position).normalized;
                 if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask))
                 {
+                    //I added this (michael), Will check this please
+                    if (target.tag == "Wall")
+                    {
+                        target.GetComponent<Wall>().Damage(100);
+                    }
+
+
                     Debug.Log(hit.collider + " and " + target);
                     if (hit.collider == target)
                     {
@@ -76,9 +84,9 @@ public class enemyCannonball : AbstractCannonball
                         {
                             target.gameObject.GetComponent<PlayerController_Rewired>().takeDamage(cannonDamage);
                         }
-                        else if (target.tag == "wall")
+                        else if (target.tag == "Wall")
                         {
-                            target.GetComponent<Wall>().Damage(cannonDamage);
+                            target.GetComponent<Wall>().Damage(100);
                         }
                         else if (target.tag == "weapon")
                         {
