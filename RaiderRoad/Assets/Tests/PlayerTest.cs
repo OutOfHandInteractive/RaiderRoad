19aaab1d0045c4b18f5f175e8b2c7256be87abea
Tobias Highfill <colossuskid@gmail.com>
2019-05-12 13:41:46 -0400

    Minor test updates

--- RaiderRoad/Assets/Tests/PlayerTest.cs
+++ RaiderRoad/Assets/Tests/PlayerTest.cs
@@ -3,39 +3,22 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using System.Linq;
 
 namespace Tests
 {
-    [TestFixture(0, 1)]
-    [TestFixture(5, 7)]
-    [TestFixture(-1, 1)]
-    public class HelloTest
+    public class PlayerTest
     {
-        private int _foo, _baz;
-
-        public HelloTest(int foo, int baz)
-        {
-            _foo = foo;
-            _baz = baz;
-        }
-
         // A Test behaves as an ordinary method
         [Test]
-        public void HelloTestSimplePasses()
+        public void PlayerTestSimplePasses()
         {
             // Use the Assert class to test conditions
-            List<int> l = new List<int>();
-            l.Add(_foo);
-            l.Add(_baz);
-            l.RemoveAll(i => i < 0);
-            Assert.That(l, Has.None.Negative);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator HelloTestWithEnumeratorPasses()
+        public IEnumerator PlayerTestWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
