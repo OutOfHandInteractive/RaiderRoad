a46c7e1cb9ce042b4eac0c6eedf72aa11625fb57
wb73-drexel <wbrown026@gmail.com>
2019-05-20 18:18:11 -0400

    KillBoxs

---
+++ RaiderRoad/Assets/Scripts/KillBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    #region System Functions
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController_Rewired playerController = collision.gameObject.GetComponent<PlayerController_Rewired>();
+            playerController.RoadRash();
+        }
+    }
+    #endregion
+}
