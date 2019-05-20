6ed8d4e99f3af385984dbc11acb7b020cb554c95
Jeffrey Mostyn <jeffmostyn2@gmail.com>
2019-05-20 18:49:47 -0400

    disabled health bar on raider death

--- RaiderRoad/Assets/Scripts/Enemy/DeathEnemy.cs
+++ RaiderRoad/Assets/Scripts/Enemy/DeathEnemy.cs
@@ -12,31 +12,24 @@ public class DeathEnemy : MonoBehaviour {
     /// </summary>
     /// <param name="enemy"></param>
     /// <param name="drop"></param>
-    public void Death(GameObject enemy, GameObject drop, ParticleSystem fx)
-    {
-		
+    public void Death(GameObject enemy, GameObject drop, ParticleSystem fx) {
         spawnDrop(drop, enemy);
         stealDrop(enemy);
 		Instantiate(fx.gameObject, transform.position, Quaternion.identity);
         Destroy(enemy);
     }
 
-    void spawnDrop(GameObject drop, GameObject enemy)
-    {
-        Debug.Log("Wall dropped!");
-        if(transform.parent != null) //REALLY SHOULDN'T STAY THIS WAY IMO
-        {
+    void spawnDrop(GameObject drop, GameObject enemy) {
+        if(transform.parent != null) { //REALLY SHOULDN'T STAY THIS WAY IMO
             GameObject item = Instantiate(drop, new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z), 
                 Quaternion.identity, transform.parent.transform);
             item.name = "Wall Drop";
         }
     }
-    void stealDrop(GameObject enemy)
-    {
-        foreach (Transform child in enemy.transform)
-        {
-            if (child.tag == "Drops")
-            {
+
+    void stealDrop(GameObject enemy) {
+        foreach (Transform child in enemy.transform) {
+            if (child.tag == "Drops") {
                 child.parent = null;
             }
         }
