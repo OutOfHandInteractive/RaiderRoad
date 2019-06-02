ba2630a6bb424f28535cfb37cb2b46c4d9df6677
Tobias Highfill <colossuskid@gmail.com>
2019-06-02 00:16:31 -0400

    Possible vehicle fix FOR REALS

--- RaiderRoad/Assets/Scripts/PlayerSpawner.cs
+++ RaiderRoad/Assets/Scripts/PlayerSpawner.cs
@@ -7,23 +7,27 @@ public class PlayerSpawner : MonoBehaviour {
 
     public GameObject SceneRV;
     private sceneManagerScript sceneManage;
+    private bool done = false;
 
     // Use this for initialization
     void Start () {
         sceneManage = sceneManagerScript.Instance;
 
         //Debug.Log(SceneRV.name);
-        Transform[] SpawnArray = new Transform[4];
-        SpawnArray[0] = SceneRV.transform.Find("SpawnPoints").Find("player1Spawn");
-        SpawnArray[1] = SceneRV.transform.Find("SpawnPoints").Find("player2Spawn");
-        SpawnArray[2] = SceneRV.transform.Find("SpawnPoints").Find("player3Spawn");
-        SpawnArray[3] = SceneRV.transform.Find("SpawnPoints").Find("player4Spawn");
-        sceneManage.SpawnPlayers(SceneRV, SpawnArray);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!done && GameManager.GameManagerInstance != null)
+        {
+            Transform[] SpawnArray = new Transform[4];
+            SpawnArray[0] = SceneRV.transform.Find("SpawnPoints").Find("player1Spawn");
+            SpawnArray[1] = SceneRV.transform.Find("SpawnPoints").Find("player2Spawn");
+            SpawnArray[2] = SceneRV.transform.Find("SpawnPoints").Find("player3Spawn");
+            SpawnArray[3] = SceneRV.transform.Find("SpawnPoints").Find("player4Spawn");
+            sceneManage.SpawnPlayers(SceneRV, SpawnArray);
+            done = true;
+        }
 	}
 }
