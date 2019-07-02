7710bf0f25c53b91e13c09a52f1bc3a68d300ba4
wb73-drexel <wbrown026@gmail.com>
2019-07-02 16:08:22 -0400

    How To Button on Main Menu

---
+++ RaiderRoad/Assets/Scripts/PressAnything.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressAnything : MonoBehaviour
+{
+    public string scene;
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
