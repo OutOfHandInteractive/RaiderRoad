8db422b3a44fd8a6bc5ef1d14cf38674032a8e82
Tobias Highfill <colossuskid@gmail.com>
2019-08-12 14:35:47 -0400

    Added navmesh and Rider class to manipulate enemies into fight state

---
+++ RaiderRoad/Assets/Scripts/Enemy/PitStopRider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class PitStopRider : MonoBehaviour
+{
+
+    private StatefulEnemyAI ai;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        ai = GetComponent<StatefulEnemyAI>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag(Constants.PLAYER_TAG).Length > 0)
+        {
+            ai.EnterFight();
+        }
+    }
+}
